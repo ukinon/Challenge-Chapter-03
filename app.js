5d64e172c2662841216924f0d324c562c2df1c96
ukino <arfianoj@gmail.com>
2023-09-21 23:52:30 +0700

    updated routings and use uuid for car id

--- app.js
+++ app.js
@@ -1,105 +1,109 @@
-const express = require('express')
-const fs = require('fs')
-const randomstring = require('randomstring')
+const express = require("express");
+const fs = require("fs");
+const uuid = require("uuid");
 
-const app = express()
+const app = express();
 const path = `${__dirname}/data/data.json`;
+const port = process.env.port || 8000;
+const cars = JSON.parse(fs.readFileSync(path));
 
-app.use(express.json())
+//middleware
+app.use(express.json());
 
-const port = process.env.port || 8000
+//functions
+const getAllCars = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: cars,
+  });
+};
 
-const cars = JSON.parse(fs.readFileSync(path))
+const getCarById = (req, res) => {
+  const id = req.params.id;
+  const car = cars.find((el) => el.id === id);
 
-app.get('/', (req, res)=>{
-    res.send('ping successfully')
-})
+  if (!car) {
+    res.status(404).json({
+      status: "failed",
+      message: `car with id ${id} not found`,
+    });
+  }
 
-app.get('/cars', (req, res)=>{
+  res.status(200).json({
+    status: "success",
+    data: car,
+  });
+};
+
+const addCar = (req, res) => {
+  const id = uuid.v4();
+  const data = Object.assign({ id: id }, req.body);
+
+  cars.push(data);
+  fs.writeFile(path, JSON.stringify(cars), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        car: data,
+      },
+    });
+  });
+};
+
+const editCar = (req, res) => {
+  const id = req.params.id;
+  const carIndex = cars.findIndex((el) => el.id === id);
+
+  if (carIndex === -1) {
+    return res.status(404).json({
+      status: "failed",
+      message: `car with id ${id} not found`,
+    });
+  }
+
+  cars[carIndex] = { ...cars[carIndex], ...req.body };
+
+  fs.writeFile(path, JSON.stringify(cars), (err) => {
     res.status(200).json({
-        status: "success",
-        data: cars
-    })
-})
+      status: "success",
+      message: `car with id ${id} has been edited successfully`,
+      data: {
+        car: cars[carIndex],
+      },
+    });
+  });
+};
 
-app.get('/cars/:id', (req, res)=>{
-    const id = req.params.id
-    const car = cars.find(el => el.id === id)
+const deleteCar = (req, res) => {
+  const id = req.params.id;
+  const carIndex = cars.findIndex((el) => el.id === id);
 
-    if(!car){
-        res.status(404).json({
-            status: 'failed',
-            message:`car with id ${id} not found`
-        })
-    }
+  if (carIndex === -1) {
+    return res.status(404).json({
+      status: "failed",
+      message: `car with id ${id} not found`,
+    });
+  }
 
+  cars.splice(carIndex, 1);
+
+  fs.writeFile(path, JSON.stringify(cars), (err) => {
     res.status(200).json({
-        status: "success",
-        data: car
-    })
-})
+      status: "success",
+      message: `car with id ${id} has been deleted successfully`,
+      data: null,
+    });
+  });
+};
 
-app.post('/cars', (req, res)=>{
-    const id = randomstring.generate()
-    const data = Object.assign({id: id}, req.body)
+//routing
+app.get("/", (req, res) => {
+  res.send("ping successfully");
+});
+app.route("/cars/").get(getAllCars).post(addCar);
+app.route("/cars/:id").get(getCarById).put(editCar).delete(deleteCar);
 
-    cars.push(data)
-    fs.writeFile(path, JSON.stringify(cars), err => {
-        res.status(201).json({
-            status: 'success',
-            data:{
-                car: data
-            }
-        })
-    })
-})
-
-app.put('/cars/:id', (req, res)=>{
-    const id = req.params.id
-    const carIndex = cars.findIndex(el => el.id === id)
-
-    if(carIndex === -1) {
-        return res.status(404).json({
-            status: 'failed',
-            message: `car with id ${id} not found`
-        })
-    }
-
-    cars[carIndex] = {...cars[carIndex], ...req.body}
-
-    fs.writeFile(path, JSON.stringify(cars), err => {
-        res.status(200).json({
-            status: 'success',
-            message: `car with id ${id} has been edited successfully`,
-            data:{
-                car: cars[carIndex]
-            }
-        })
-    })
-})
-
-app.delete('/cars/:id', (req, res)=>{
-    const id = req.params.id
-    const carIndex = cars.findIndex(el => el.id === id)
-
-    if(carIndex === -1) {
-        return res.status(404).json({
-            status: 'failed',
-            message: `car with id ${id} not found`
-        })
-    }
-
-    cars.splice(carIndex, 1)
-
-    fs.writeFile(path, JSON.stringify(cars), err => {
-        res.status(200).json({
-            status: 'success',
-            message: `car with id ${id} has been deleted successfully`,
-            data:null
-        })
-    })
-})
-
+//listen
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
+  console.log(`App running on port ${port}...`);
+});
